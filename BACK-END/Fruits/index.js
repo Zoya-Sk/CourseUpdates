f0013349f2fd18a1b18c5ae23bd06af9dac6ab23
Zoya-Sk <sakinask2006@gmail.com>
2025-03-07 14:59:22 +0530

    Initializing node - EJS

--- BACK-END/Fruits/index.js
+++
@@ -1,7 +0,0 @@
-const apple = require("./apple");
-const banana = require("./banana");
-const mango = require("./mango");
-
-let fruits = [apple, mango, banana];
-
-module.exports = fruits;
--- BACK-END/Fruits/mango.js
+++
@@ -1,6 +0,0 @@
-const { color } = require("./apple");
-
-module.exports = {
-    name: "mango",
-    color: "yellow-orange"
-};
