aa0d739266101a374505abe18da82bbf562d1565
Zoya-Sk <sakinask2006@gmail.com>
2025-05-07 12:42:45 +0530

    updated part 10 script.js

--- JAVASCRIPT/JSpart10/scripts.js
+++ JAVASCRIPT/JSpart10/scripts.js
@@ -3,3 +3,12 @@ let btn = document.querySelector('button');
 btn.onclick = function(){
     console.log("Clicked.");
 }
+
+function sayHello(){
+    alert("Hello!");
+}
+btn.onclick = sayHello
+
+btn.onmouseenter = function(){
+    console.log("You entered a button.");
+}
