402da0a2eac005089f6a4f8e35e9580c51f04054
Zoya-Sk <sakinask2006@gmail.com>
2025-05-12 17:41:52 +0530

    updates with concepts.

---
+++ Miscellaneous/frontend/app.js
@@ -0,0 +1,25 @@
+const student = {
+    name: "shradha",
+    age: 24,
+    marks: 90,
+    getMarks: function(){
+        return this.marks
+    }
+}
+const student2 = {
+    name: "aman",
+    age: 26,
+    marks: 92.40,
+    getMarks: function(){
+        return this.marks
+    }
+}
+const student3 = {
+    name: "rajat",
+    age: 25,
+    marks: 90.25,
+    getMarks: function(){
+        return this.marks
+    }
+}
+// Inefficiency due to repeated key values -> better use OOPs
